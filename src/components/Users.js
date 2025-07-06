383b08823eeb21fe7b55ed008938450343199ece
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-07-06 17:31:59 +0530

    refactor: Convert Users component to class-based implementation

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import {Components ,useState } from 'react';
+import { Component } from 'react'; // useState මෙතන අවශ්‍ය නැහැ, මොකද Class Component එකක් නිසා
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,40 +9,41 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
+class Users extends Component {
 
-class Users extends Component{
+  constructor(props) {
+   
+    super(props); 
+    this.state = {
+      showUsers: true
+    };
+  }
 
-  render(){
+
+  toggleUsersHandler = () => { 
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers }
+    });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
 
     return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler}> 
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
   }
-  
 }
-
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
-
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  
-};
 
 export default Users;
